e7823163b07834476f2a062f81ac5170aca72505
Carsten Bach <mail@carsten-bach.de>
2024-10-21 00:59:22 +0200

    WIP

---
+++ themes/jr24/patterns/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: footer
+ * Slug: jr24/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"quote-with-image"} /-->
+
+<!-- wp:template-part {"slug":"contact-widget"} /-->
+
+<!-- wp:template-part {"slug":"awarded-supported-by"} /-->
+
+<!-- wp:template-part {"slug":"active-engaged"} /-->
+
+<!-- wp:group {"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}}}} -->
+<div class="wp-block-group" style="padding-top:80px;padding-bottom:30px"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php echo __('Proudly Powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a>', 'jr24');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
